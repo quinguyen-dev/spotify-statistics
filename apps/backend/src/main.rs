f7c157ef13e81cb0a4607325d3f3b0feea1a1289
Qui Nguyen <quinguyen.dev@gmail.com>
2024-07-08 11:25:37 -0400

    [📝] Integrated Docker and containerized Rust microservice.

--- apps/backend/src/main.rs
+++ apps/backend/src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, post, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+#[get("/healthcheck")]
+async fn healthcheck() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+            .service(healthcheck)
+    })
+    .bind(("0.0.0.0", 3001))?
+    .run()
+    .await
 }
